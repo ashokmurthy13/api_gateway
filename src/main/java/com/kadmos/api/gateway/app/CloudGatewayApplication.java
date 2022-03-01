a2045170ef0c6b7ba3cb549d8b990af248094813
Ashok Murthy <ashokmurthy88@gmail.com>
2022-03-01 11:45:27 +0900

    updated README file

--- src/main/java/com/kadmos/api/gateway/app/CloudGatewayApplication.java
+++ src/main/java/com/kadmos/api/gateway/app/CloudGatewayApplication.java
@@ -2,10 +2,12 @@ package com.kadmos.api.gateway.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableWebFluxSecurity
+@EnableEurekaClient
 public class CloudGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudGatewayApplication.class, args);
